aef9c44749acdec7a42144121f60d2c0988ffd8d
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-11-19 09:32:33 +0100

    update

---
+++ index.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Tyrolium Site</title>
+    <link href="https://tyrolium.fr/Contenu/Image/Tyrolium Site.png" rel="shortcut icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Oxanium&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+
+
+    <link rel="stylesheet" type="text/css" media="screen" href="http://tyroserv.fr/all.css">
+
+
+</head>
+<body>
+<header>
+    <header>
+        <div class="container text-white py-4">
+            <nav class="navbar navbar-expand-lg navbar-dark ">
+                <a class="navbar-brand" href="#"><img src="http://tyrolium.fr/Contenu/Image/Tyrolium Site.png" alt="logo"></a>
+                <button class="navbar-toggler border-0" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#objectif">Objectifs</a>
+                        </li>
+                        <li class="nav-item ms-lg-4">
+                            <a class="nav-link active" href="#comment">Jouer</a>
+                        </li>
+                        <li class="nav-item ms-lg-4">
+                            <a class="nav-link active" href="#inscription">S'inscrire</a>
+                        </li>
+                        <li class="nav-item ms-lg-4">
+                            <a class="nav-link active" href="#téléchargement">Téléchargement</a>
+                        </li>
+                        <li class="nav-item ms-lg-4">
+                            <a class="nav-link active" href="#nosreseau">Nos Réseaux</a>
+                        </li>
+                        <li class="nav-item ms-lg-4">
+                            <a target="_blank" class="nav-link active" href="http://tyrolium.tebex.io/">Boutique</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <h1 class="text-center display-1">UN TITRE</h1>
+        </div>
+    </header>
+</header>
+
+
+
+
+
+
+
+
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
