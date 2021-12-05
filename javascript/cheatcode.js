a749bbd7ae480254741d0496f35997d051457786
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-12-05 01:41:45 +0100

    timeline update + esteregg

---
+++ javascript/cheatcode.js
@@ -0,0 +1,88 @@
+var konamiCode = "↑↑↓↓←→←→AB";
+var cmd = null
+
+$(document).keyup(function (e) {
+
+    if (e.keyCode == 37 || e.keyCode == 38 || e.keyCode == 39 || e.keyCode == 40 || e.keyCode == 65 || e.keyCode == 66) {
+        cheatcode("key" + e.keyCode + "|");
+    }
+
+});
+
+
+function cheatcode(arg) {
+
+    if(arg == "key38|"){
+        if(cmd == null){
+            cmd = "↑";
+        } else if (cmd == "↑"){
+            cmd = cmd + "↑";
+        } else {
+            cmd = null;
+        }
+    }
+
+    if(arg == "key40|"){
+        if(cmd == "↑↑"){
+            cmd = "↑↑↓";
+        } else if (cmd == "↑↑↓"){
+            cmd = cmd + "↓";
+        } else {
+            cmd = null;
+        }
+    }
+
+    if(arg == "key37|"){
+        if(cmd == "↑↑↓↓"){
+            cmd = "↑↑↓↓←";
+        } else if (cmd == "↑↑↓↓←→"){
+            cmd = cmd + "←";
+        } else {
+            cmd = null;
+        }
+    }
+
+    if(arg == "key39|"){
+        if(cmd == "↑↑↓↓←"){
+            cmd = "↑↑↓↓←→";
+        } else if (cmd == "↑↑↓↓←→←"){
+            cmd = cmd + "→";
+        } else {
+            cmd = null;
+        }
+    }
+
+    if(arg == "key65|"){
+        if(cmd == "↑↑↓↓←→←→"){
+            cmd = "↑↑↓↓←→←→A";
+        } else {
+            cmd = null;
+        }
+    }
+
+    if(arg == "key66|"){
+        if(cmd == "↑↑↓↓←→←→A"){
+            cmd = "↑↑↓↓←→←→AB";
+        } else {
+            cmd = null;
+        }
+    }
+
+    if(cmd){
+        console.log(cmd);
+    }
+
+    if(arg == "konamicode" || cmd == konamiCode){
+        alert("GG!");
+        cmd = null;
+    }
+    if(arg == "flappybird"){
+        window.location.href='https://themaxium69.github.io/Flappium/'
+    }
+    if(arg == "clavier"){
+        window.location.href='https://themaxium69.github.io/Clavium/'
+    }
+    if(arg == "echec"){
+        window.location.href='https://themaxium69.github.io/Echec-ium/'
+    }
+}
--- story.php
+++ story.php
@@ -1,20 +1,80 @@
 <?php include "app/app.php"; $page = 5; head($page); ?>
 <body id="story"> <header> <?php navbar($page); ?> </header>
 
+<section id="storyhead">
+    <br>
+    <h2 style="text-align: center">Notre Histoire</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla .</p>
+    <br>
+    <hr style="background: rgb(161, 161, 161);">
+    <br>
+</section>
 <main id="story">
-
     <section class="timeline">
         <div class="container">
             <div class="timeline-item">
                 <div class="timeline-img"></div>
 
                 <div class="timeline-content js--fadeInLeft">
-                    <h2>Title</h2>
-                    <div class="date">1 MAY 2016</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
+                    <h2>Création du projet</h2>
+                    <div class="date">9 juillet 2017</div>
+                    <p>Imaginez comme un serveur minecraft dit "PVP/Faction Moddé"</p>
                     <a class="bnt-more" href="javascript:void(0)">More</a>
                 </div>
             </div>
+
+            <div class="timeline-item">
+
+                <div class="timeline-img"></div>
+
+                <div class="timeline-content js--fadeInRight">
+                    <h2>Invention du nom "Tyrolium"</h2>
+                    <div class="date">12 juillet 2017</div>
+                    <p>Le nom "Tyrolium" est à l'origine une contraction d'un mineraix et d'un metal : la "Tyrolite" et le "Paladium", Le "Tyrolium" avant tout un mineraix créer pour notre Serveur minecraft</p>
+                    <a class="bnt-more" href="javascript:void(0)">More</a>
+                </div>
+            </div>
+
+            <div class="timeline-item">
+                <div class="timeline-img"></div>
+
+                <div class="timeline-content js--fadeInLeft">
+                    <h2>Création de Tyrolium Ent.</h2>
+                    <div class="date">29 Juillet 2019</div>
+                    <p>Lorem Ipsu</p>
+                    <a class="bnt-more" href="javascript:void(0)">More</a>
+                </div>
+            </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
             <div class="timeline-item">
 
@@ -118,6 +178,19 @@
                     <a class="bnt-more" href="javascript:void(0)">More</a>
                 </div>
             </div>
+
+            <div class="timeline-item">
+
+                <div class="timeline-img"></div>
+
+                <div class="timeline-content js--fadeInRight">
+                    <div class="date">1 SEP 2016</div>
+                    <h2>Title</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime ipsa ratione omnis alias cupiditate saepe atque totam aperiam sed nulla voluptatem recusandae dolor, nostrum excepturi amet in dolores. Alias, ullam.</p>
+                    <a class="bnt-more" href="javascript:void(0)">More</a>
+                </div>
+            </div>
+
 
 
 
