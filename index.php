15ecea06e466fec2a57436525ea888f6f1034308
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-12-30 22:16:25 +0100

    final home page mobil

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         </div>
     </section>
 
-
+    <!-- PC -->
     <div class="home-desktop">
 
     <section id="home-tyro" class="container row">
@@ -87,6 +87,7 @@
     </section>
 
         <?php if($env_isAccount == true) { ?>
+            <!-- PC - ACCOUNT -->
 
     <section id="home-account">
         <div class="container row">
@@ -108,10 +109,13 @@
     <section id="home-story" class="container row">
         <div class="container col-4">
             <article>
-                <h2 class="art">Histoire</h2>
-                <p class="art">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+                <h2 class="art">Notre Histoire</h2>
+                <p class="art">Voilà depuis 2017 que Tyrolium existe et fait mûrir des projets et les gens qui ont travaillés et travaillent encore dessus.
+                    Pendant ces 4 années, une évolution s'est faite sentir et nous allons détailler cette évolution en historique et vous raconter les points clés de celle-ci.
+
+                    N'oubliez pas cependant que l'évolution n'est pas la fin, elle n'est que le début de la poussée qui nous emmènera au sommet, tant que l'on peux faire mieux ça ne sera jamais terminé.</p>
                 <div style="text-align: center">
-                    <?php tyrobtn("story", "story", "Plus d'info");?>
+                    <?php tyrobtn("story", "story", "Voir l'Histoire de Tyrolium");?>
                 </div>
             </article>
         </div>
@@ -126,13 +130,19 @@
         </div>
         <div class="container col-4">
             <article>
-                <h2 class="art">France</h2>
-                <p class="art">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
-            </article>
+                <h2 class="art">Indépendant et Français</h2>
+                <p class="art">Le but de Tyrolium est d'exploiter au mieux le potentiel de ses employés pour créer des projets toujours plus ambitieux.
+                    Les fonds générés par nos prestations sont injectés dans l'entreprise pour financer des projets.
+                </p><p class="art">
+                    L'objectif de Tyrolium est de concurrencer le monde et les géants de notre domaine en restant français.
+                </p><p class="art">
+                    Fait en France, par des français.</p></article>
         </div>
     </section>
 
         <?php } else { ?>
+
+    <!-- PC - NO ACCOUNT -->
 
         <section id="home-account">
             <div class="container row" style="padding-bottom: 30px">
@@ -154,7 +164,7 @@
             </div>
         </section>
 
-        <section id="home-story" class="container row">
+        <section id="home-french" class="container row">
             <div class="container col-4">
                 <article>
                     <h2 class="art">Indépendant et Français</h2>
@@ -172,36 +182,180 @@
             </div>
         </section>
     <?php } ?>
-
     </div>
 
+    <!-- MOBILE -->
+
     <div class="home-res">
-        <section id="home-service" class="container">
+
+        <section id="home-tyro" class="container">
             <div class="container col-4">
                 <article>
-                    <h2 class="art-res">Service</h2>
-                    <p class="art-res">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+                    <h2 class="art-res">A propos de Tyrolium</h2>
+                    <p class="art-res">Tyrolium est une entreprise multifonction qui travaille sur plusieurs projets, notamment du développement de jeux vidéos ou d'applications web, de la musique, ou de l'audiovisuel.</p>
+                    <br>
+                    <p class="art-res">Elle propose principalement des prestations de services de création de site Web et d'applications pour les particuliers et les professionnels.</p>
+                    <br>
+                    <p class="art-res">Les profits générés par ces prestations seront directement investies dans les projets de l'entreprise.</p>
                     <div>
                         <div class="" style="width: 60%; margin-left: 20%">
-                            <img src="assets/Team_work_Monochromatic.png" width="100%" height="100%">
-                        </div>
-                        <div class="" style="text-align: center; margin-top: 15px" >
-                            <?php tyrobtn("service", "service", "Plus d'info");?>
-                            <style>
-                                .tyrobutton{
-                                    width: 100%;
-                                }
-                            </style>
+                            <img src="assets/Tyrolium_home.png" width="100%" height="100%">
                         </div>
                     </div>
                 </article>
             </div>
         </section>
+
+
+        <hr style="background: rgb(161, 161, 161); margin-top: 40px; margin-bottom: 40px">
+
+
+                <section id="home-service" class="container">
+                    <div class="container col-4">
+                        <article>
+                            <h2 class="art-res">Nos Prestations</h2>
+                            <p class="art-res">Tyrolium vous propose des prestations de services, notamment dans le développement de sites Web, d'applications, l'hébergement de serveurs, le développement minecraft et des bots (discord ou autre) pour les particuliers et les professionnels. Les fonds générés par ces prestations seront investis dans les projets de l'entreprise.</p>
+                            <div>
+                                <div class="" style="width: 60%; margin-left: 20%">
+                                    <img src="assets/Team_work_Monochromatic.png" width="100%" height="100%">
+                                </div>
+                                <div class="" style="text-align: center; margin-top: 15px" >
+                                    <?php tyrobtn("service", "service", "Liste de nos Prestations");?>
+                                    <style>
+                                        .tyrobutton{
+                                            width: 100%;
+                                        }
+                                    </style>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </section>
+
+                <section id="home-project" class="container">
+                    <div class="container col-4">
+                        <article>
+                            <h2 class="art-res">Nos Projets</h2>
+                            <p class="art-res">De nos jours, nous sommes beaucoup à rêver de créer nos propres projets. Tyrolium a réussi le pari de réaliser les rêves de nos employés passionnés.
+                                Nous sommes fiers de ce que nous accomplissons tous les jours.
+                                Nous vous proposons des projets de grande ampleur toujours avec la passion de tous nos équipes.
+                                Tyrolium veut vous proposer des services et des projets bien realisés, de quoi vous divertir ou vous simplifier la vie.</p>
+                            <div>
+                                <div class="" style="width: 60%; margin-left: 20%">
+                                    <img src="assets/Startup_Monochromatic.png" width="100%" height="100%">
+                                </div>
+                                <div class="" style="text-align: center; margin-top: 15px" >
+                                    <?php tyrobtn("project", "project", "Tout nos Projets");?>
+                                    <style>
+                                        .tyrobutton{
+                                            width: 100%;
+                                        }
+                                    </style>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </section>
+
+
+                <section id="home-collaboration" class="container">
+                    <div class="container col-4">
+                        <article>
+                            <h2 class="art-res">Nos Collaborations</h2>
+                            <p class="art-res">Tyrolium est une entreprise qui est formée de plusieurs équipes, elle sont dirigés principalement dans les projets de l'entreprise ainsi que les prestations proposées.
+                                Ces équipes sont formées de personnes motivées, dynamiques et investies dans les projets et dans l'entreprise.
+
+                                Nous recrutons tous types de profils, les seuls pré-requis sont d'être intéressé et motivé par les projets qui vous attires. </p>
+                            <div>
+                                <div class="" style="width: 60%; margin-left: 20%">
+                                    <img src="assets/Brainstorming_session_Monochromatic.png" width="100%" height="100%">
+                                </div>
+                                <div class="" style="text-align: center; margin-top: 15px" >
+                                    <?php tyrobtn("collaboration", "collaboration", "Nos Collaborations");?>
+                                    <style>
+                                        .tyrobutton{
+                                            width: 100%;
+                                        }
+                                    </style>
+                                </div>
+                            </div>
+                        </article>
+                    </div>
+                </section>
+
+
+        <?php if($env_isAccount == true) { ?>
+            <!-- MOBILE - ACCOUNT -->
+
+            <section id="home-account" class="container">
+                <div class="container col-4">
+                    <article>
+                        <h2 class="art-res">Account</h2>
+                        <p class="art-res">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,</p>
+                        <div>
+                            <div class="" style="width: 60%; margin-left: 20%">
+                                <img src="assets/Information_flow_Monochromatic.png" width="100%" height="100%">
+                            </div>
+                            <div class="" style="text-align: center; margin-top: 15px" >
+                                <?php tyrobtn("account", "account", "Plus d'info");?>
+                                <style>
+                                    .tyrobutton{
+                                        width: 100%;
+                                    }
+                                </style>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </section>
+
+        <?php } ?>
+
+            <section id="home-story" class="container">
+                <div class="container col-4">
+                    <article>
+                        <h2 class="art-res">Notre Histoire</h2>
+                        <p class="art-res">Voilà depuis 2017 que Tyrolium existe et fait mûrir des projets et les gens qui ont travaillés et travaillent encore dessus.
+                            Pendant ces 4 années, une évolution s'est faite sentir et nous allons détailler cette évolution en historique et vous raconter les points clés de celle-ci.
+
+                            N'oubliez pas cependant que l'évolution n'est pas la fin, elle n'est que le début de la poussée qui nous emmènera au sommet, tant que l'on peux faire mieux ça ne sera jamais terminé. </p>
+                        <div>
+                            <div class="" style="width: 60%; margin-left: 20%">
+                                <img src="assets/Timeline_Monochromatic.png" width="100%" height="100%">
+                            </div>
+                            <div class="" style="text-align: center; margin-top: 15px" >
+                                <?php tyrobtn("story", "story", "Voir l'Histoire de Tyrolium");?>
+                                <style>
+                                    .tyrobutton{
+                                        width: 100%;
+                                    }
+                                </style>
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </section>
+
+            <section id="home-french" class="container">
+                <div class="container col-4">
+                    <article>
+                        <h2 class="art-res">Indépendant et Français</h2>
+                        <p class="art-res">Le but de Tyrolium est d'exploiter au mieux le potentiel de ses employés pour créer des projets toujours plus ambitieux.
+                            Les fonds générés par nos prestations sont injectés dans l'entreprise pour financer des projets.</p>
+                        <p class="art-res">
+                            L'objectif de Tyrolium est de concurrencer le monde et les géants de notre domaine en restant français.
+                        </p><p class="art-res">
+                            Fait en France, par des français.</p>
+                        <div>
+                            <div class="" style="width: 60%; margin-left: 20%">
+                                <img src="assets/Paris_Monochromatic.png" width="100%" height="100%">
+                            </div>
+                        </div>
+                    </article>
+                </div>
+            </section>
+
     </div>
-
-
-
-
 </main>
 
 <?php footer(); ?> </body> </html>
