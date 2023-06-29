840e3f03eae4c61612168c4533f068cacb0a50a1
Maxime Tournier <mxm.tournier@gmail.comls>
2023-06-29 18:56:50 +0200

    update

--- service.php
+++ service.php
@@ -12,7 +12,7 @@
     </section>
     <section id="service-start" class="row container">
         <div class="col-9">
-            <h2 class="titleS">Site Web</h2>
+            <h2 class="titleS"><i class="fas fa-caret-right" style="  color: #1325d7;"></i> Site Web</h2>
             <br id="991none">
             <p class="container">Ci-dessous les différentes formules sont affichées,<br> nous faisons majoritairement de la création de sites web.</p>
             <br>
@@ -30,6 +30,9 @@
                     <div class="pricing-plan">
                         <img src="assets/paper-plane.png" alt="PaperPlane" class="pricing-img">
                         <h2 class="pricing-header"><?php echo $env_presta_web['0'] ?></h2>
+                        <br>
+                        <p class="pricing-header" style="margin-bottom: -10px; color: #888;">à partir de </p>
+                        <span class="pricing-price">50€</span>
                         <ul class="pricing-features">
                             <li class="pricing-features-item"><div><span class="underline bold">Concrètement</span> : Il s'agit d'une formule bien pour les porte-folios.
                                 Les sites de présentation juste avec du texte pour présenter un projet.</div>
@@ -44,6 +47,9 @@
                     <div class="pricing-plan">
                         <img src="assets/plane.png" alt="Plane" class="pricing-img">
                         <h2 class="pricing-header"><?php echo $env_presta_web['1'] ?></h2>
+                        <br>
+                        <p class="pricing-header" style="margin-bottom: -10px; color: #888;">à partir de </p>
+                        <span class="pricing-price">400€</span>
                         <ul class="pricing-features">
                             <li class="pricing-features-item"><div><span class="underline bold">Concrètement</span> : Il s'agit d'une formule bien pour les entreprises, les sites qui demande plus qu'un simple texte mais une présention qui pourra afficher des informations. Avec cette formule le site pourra être modifier grâce à un panel administrateur.</div></li>
                             <li class="pricing-features-item"><div><span class="underline bold">Techniquement</span> : Il s'agit de plusieurs pages qui pourra être modifiable, on pourra se connecter à des API simple, où l'on utilisera plus régulièrement des FrameWork front-end, avec Php-Object.</div></li>
@@ -55,6 +61,9 @@
                     <div class="pricing-plan">
                         <img src="assets/space-ship.png" alt="SpaceShip" class="pricing-img">
                         <h2 class="pricing-header"><?php echo $env_presta_web['2'] ?></h2>
+                        <br>
+                        <p class="pricing-header" style="margin-bottom: -10px; color: #888;">à partir de </p>
+                        <span class="pricing-price">1600€</span>
                         <ul class="pricing-features">
                             <li class="pricing-features-item"><div><span class="underline bold">Concrètement</span> : Il s'agit d'une bonne formule notamment pour un gros projet,
                                 créer un réseau social, créer une boutique en ligne, un blog ou une application de gestion personnalisée.</div></li>
@@ -114,11 +123,12 @@
             </div>
         </div>
         <br>
-        <button style="font-size: 13px;" type="button" id="btn-contact" class="tyrobuttonNone popup-btn">Voir nos réalisations</button>
+        <h2 class="titleS" style="text-align: center; font-size:18px;"><i class="fas fa-caret-right" style="  color: #1325d7;"></i> Les sites déjà réaliser par Tyrolium</p>
+        <br>
+        <button style="font-size: 18px;" type="button" id="btn-contact" class="tyrobuttonNone popup-btn">Voir nos réalisations</button>
         <script src="javascript/popup.js"></script>
         <br>
         <br>
-        <p style="text-align: center">Tout l'argent que vous reversez à Tyrolium sera investi dans les projets de l'entreprise.</p>
         <br>
         <hr style="background: rgb(161, 161, 161);">
         <br>
@@ -129,7 +139,7 @@
     <!-- PC -->
     <div class="home-desktop">
 
-        <section id="service-second" class="container row">
+        <!-- <section id="service-second" class="container row">
             <div class="col-6" style="text-align: end">
                 <img class="img-home" src="assets/Designer%20_Monochromatic.png" style="width: 100%; height: 100%">
             </div>
@@ -142,11 +152,11 @@
                     Nous sommes dans la capacité de vous aider à choisir et réaliser votre branding, votre charte graphique et vos couleurs.
                     Cette prestation sers avant tout à vous accompagner dans les choix graphiques de votre projet.</p>
                     <div style="text-align: center">
-                        <?php tyrobtn("contact.php?name=graph", "contact", "Contactez-nous pour cette prestation");?>
+                        <?php //tyrobtn("contact.php?name=graph", "contact", "Contactez-nous pour cette prestation");?>
                     </div>
                 </article>
             </div>
-        </section>
+        </section> -->
 
         <!-- <section id="service-second-but" class="container row">
             <div class="container col-5">
@@ -167,32 +177,12 @@
             </div>
         </section> -->
         
-        <!-- <section id="service-second" class="container row">
-            <div class="col-6" style="text-align: end">
-                <img class="img-home" src="assets/User%20interface_Monochromatic.png" style="width: 96%; height: 100%">
-            </div>
-            <div class="container col-5">
-                <article id="art-home">
-                    <h2 class="art">Montage Vidéo / Animation</h2>
-                    <p class="art">
-                    Nos équipes peuvent vous aider dans vos projets audiovisuel.
-Vous voulez impacter vos client avec une animation ou un montage vidéo ?
-<br>
-Vous êtes un vidéaste plein d'idées et vous voulez un monteur qui vous simplifie votre travail ?
-<br>
-Avec cette prestation nos équipes pourront vous accompagner dans vos projets et réaliser vos animations, vidéos promotionnelles ou vos vidéos personnelles pour votre propre projet.
-                    </p>
-                    <div style="text-align: center">
-                        <?php //tyrobtn("contact.php?name=anim", "contact", "Contactez-nous pour cette prestation");?>
-                    </div>
-                </article>
-            </div>
-        </section> -->
+        
 
-        <section id="service-second-but" class="container row">
+        <section id="service-second-but" class="container row service-ecart">
             <div class="container col-5">
                 <article>
-                    <h2 class="art">Minecraft</h2>
+                    <h2 class="art"><i class="fas fa-caret-right"></i> Minecraft</h2>
                     <p class="art">Prestation où nous réalisons le développement de votre mod ou de votre plugin.<br>
                         Nous pouvons également configurer votre serveur, nos équipes ont beaucoup travaillé avec ce jeu et nous sommes en mesure de réaliser toutes vos demandes concernant celui-ci.</p>
                     <div style="text-align: center">
@@ -205,13 +195,13 @@ Avec cette prestation nos équipes pourront vous accompagner dans vos projets et
             </div>
         </section>
 
-        <section id="service-second-but" class="container row">
+        <section id="service-second-but" class="container row service-ecart">
             <div class="col-6">
                 <img class="img-home" src="assets/Online%20storage_Monochromatic.png" style="width: 100%;">
             </div>
             <div class="container col-5">
                 <article id="art-home">
-                    <h2 class="art">Configurations Serveur</h2>
+                    <h2 class="art"><i class="fas fa-caret-right"></i> Configurations Serveur</h2>
                     <p class="art">Nous pouvons vous conseiller ou/et vous configurer vos serveurs.
                         Linux est parfois compliqué à comprendre.
                         Nous pouvons installer et mettre en place toutes vos demandes sur vos serveurs en fonction de vos besoins. </p>
@@ -222,21 +212,66 @@ Avec cette prestation nos équipes pourront vous accompagner dans vos projets et
             </div>
         </section>
 
-        <!-- <section id="service-second-but" class="container row">
+        <div id="service-solidserv-fleche"></div>
+        
+        <section id="service-solidserv" class="service-ecart"> 
+            <div class="container row">
+                <div class="col-6" style="text-align: center;">        
+                    <a href="">
+                        <img class="img-solidserv img-home" src="assets/Information_flow_Monochromatic.png">
+                    </a>
+                </div>
+                <div class="container col-5">
+                    <article>
+                        <h2 class="art"><i class="fas fa-caret-right"></i> Avec SolidServ</h2>
+                        <p class="art" style='    padding-bottom: 10px;
+        margin-top: -21px;'>Beneficier de 50% de réduction sur tout les serveur de <strong>SolidServ</strong>, notre propre hébergeur serveur créer et porté par Tyrolium et ses équipes</p>
+                        <div style="text-align: center">
+                            <?php tyrobtn("https://solidserv.fr", "solidserv", "Site de SolidServ");?>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
+
+
+        <section id="service-second-but" class="container row service-ecart">
             <div class="container col-5">
                 <article>
-                    <h2 class="art">Bot Discord</h2>
+                    <h2 class="art"><i class="fas fa-caret-right"></i> Bot Discord</h2>
                     <p class="art">Nous sommes capable de vous créer votre bot discord, autant pour automatiser ou personnaliser les moindres fonctionnalités de vos serveurs discord.
                         Nous pouvons aussi le lier à des API et autres logiciels ou applications.</p>
                     <div style="text-align: center">
-                        <?php //tyrobtn("contact.php?name=serv", "contact", "Contactez-nous pour cette prestation");?>
+                        <?php tyrobtn("contact.php?name=serv", "contact", "Contactez-nous pour cette prestation");?>
                     </div>
                 </article>
             </div>
             <div class="col-6" style="text-align: end">
                 <img class="img-home" src="assets/Social%20Media_Monochromatic.png" style="width: 96%; height: 100%">
             </div>
-        </section> -->
+        </section>
+
+        <section id="service-second" class="container row" style="margin-bottom: 100px">
+            <div class="col-6" style="text-align: end">
+                <img class="img-home" src="assets/User%20interface_Monochromatic.png" style="width: 96%; height: 100%">
+            </div>
+            <div class="container col-5">
+                <article id="art-home">
+                    <h2 class="art"><i class="fas fa-caret-right"></i> Montage Vidéo / Animation</h2>
+                    <p class="art">
+                    Nos équipes peuvent vous aider dans vos projets audiovisuel.
+Vous voulez impacter vos client avec une animation ou un montage vidéo ?
+<br>
+Vous êtes un vidéaste plein d'idées et vous voulez un monteur qui vous simplifie votre travail ?
+<br>
+Avec cette prestation nos équipes pourront vous accompagner dans vos projets et réaliser vos animations, vidéos promotionnelles ou vos vidéos personnelles pour votre propre projet.
+                    </p>
+                    <div style="text-align: center">
+                        <?php tyrobtn("contact.php?name=anim", "contact", "Contactez-nous pour cette prestation");?>
+                    </div>
+                </article>
+            </div>
+        </section>
 
 
 
@@ -246,7 +281,7 @@ Avec cette prestation nos équipes pourront vous accompagner dans vos projets et
 
     <div class="home-res">
 
-    <section id="service-second">
+    <!-- <section id="service-second">
         <h2>Graphisme</h2>
         <p>
                     Dans ce domaine très important pour vos projets, nous sommes capables de vous faire l'entièreté de votre identité visuelle.
@@ -259,9 +294,9 @@ Avec cette prestation nos équipes pourront vous accompagner dans vos projets et
                             </div>
                         </div>
                     <br>
-        <?php tyrobtn("contact.php?name=graph", "contact", "Contactez-nous pour cette prestation");?>
+        <?php //tyrobtn("contact.php?name=graph", "contact", "Contactez-nous pour cette prestation");?>
                         
-    </section>
+    </section> -->
 
     <!-- <section id="service-second-but">
         <h2>Musicales</h2>
@@ -276,24 +311,6 @@ Avec cette prestation nos équipes pourront vous accompagner dans vos projets et
                         </div>
                     <br>
         <?php //tyrobtn("contact.php?name=music", "contact", "Contactez-nous pour cette prestation");?>
-    </section> -->
-        
-    <!-- <section id="service-second-but">
-        <h2>Montage Vidéo / Animation</h2>
-        <p>
-        Nos équipes peuvent vous aider dans vos projets audiovisuel.
-Vous voulez impacter vos client avec une animation ou un montage vidéo ?
-<br>
-Vous êtes un vidéaste plein d'idées et vous voulez un monteur qui vous simplifie votre travail ?
-<br>
-Avec cette prestation nos équipes pourront vous accompagner dans vos projets et réaliser vos animations, vidéos promotionnelles ou vos vidéos personnelles pour votre propre projet.
-        </p><div>
-                            <div class="" style="width: 60%; margin-left: 20%">
-                                <img src="assets/User%20interface_Monochromatic.png" width="100%" height="100%">
-                            </div>
-                        </div>
-                    <br>
-        <?php //tyrobtn("contact.php?name=anim", "contact", "Contactez-nous pour cette prestation");?>
     </section> -->
 
     <section id="service-second-but">
@@ -334,6 +351,23 @@ Avec cette prestation nos équipes pourront vous accompagner dans vos projets et
         <?php tyrobtn("contact.php?name=bot", "contact", "Contactez-nous pour cette prestation");?>
     </section>
     
+    <section id="service-second-but">
+        <h2>Montage Vidéo / Animation</h2>
+        <p>
+        Nos équipes peuvent vous aider dans vos projets audiovisuel.
+Vous voulez impacter vos client avec une animation ou un montage vidéo ?
+<br>
+Vous êtes un vidéaste plein d'idées et vous voulez un monteur qui vous simplifie votre travail ?
+<br>
+Avec cette prestation nos équipes pourront vous accompagner dans vos projets et réaliser vos animations, vidéos promotionnelles ou vos vidéos personnelles pour votre propre projet.
+        </p><div>
+                            <div class="" style="width: 60%; margin-left: 20%">
+                                <img src="assets/User%20interface_Monochromatic.png" width="100%" height="100%">
+                            </div>
+                        </div>
+                    <br>
+        <?php tyrobtn("contact.php?name=anim", "contact", "Contactez-nous pour cette prestation");?>
+    </section>
 
     </div>
     <br><br>
