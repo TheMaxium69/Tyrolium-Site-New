db768cda6dffd2312570cbed7e6f57a2c1ebb205
TheMaxium69 <mxm.tournier@gmail.com>
2025-03-18 15:48:15 +0100

    template and meta update

---
+++ lang/template.lang.php
@@ -0,0 +1,657 @@
+<?php
+
+$lang = array(
+
+    /* PAGE NAME */
+    "page" => array(
+
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        ""
+
+    ),
+
+    /* SERVICE PAGE NAME */
+    "service-page" => array(
+        array(
+            "name" => "",
+            "sub" => '',
+        ),
+        array(
+            "name" => "",
+            "sub" => ""
+        ),
+        array(
+            "name" => "",
+            "sub" => '',
+        ),
+        array(
+            "name" => "CRM",
+            "sub" => ''
+        ),
+    ),
+
+    /* FOOTER */
+    "footer" => array(
+
+        "title" => array(
+
+            "",
+            "",
+            ""
+
+        ),
+
+        "other-page" => array(
+
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+
+        ),
+
+        "about-text" => ""
+
+    ),
+
+    /* HOME */
+    1 => array(
+
+        "head" => "",
+
+        "about" => array(
+
+            "title" => "",
+            "txt" => "",
+            "mobile" => ""
+
+
+        ),
+
+        "service" => array(
+
+            "title" => "",
+            "btn1" => "",
+            "btn2" => "",
+            "btn3" => "",
+            "txt" => ""
+        ),
+
+        "project" => array(
+
+            "title" => "",
+            "btn" => "",
+            "txt" => ""
+
+        ),
+
+        "collab" => array(
+
+            "title" => "",
+            "btn" => "",
+            "txt" => ""
+
+        ),
+
+        "account" => array(
+
+            "title" => "",
+            "btn" => "",
+            "txt" => ""
+
+        ),
+
+        "story" => array(
+
+            "title" => "",
+            "btn" => "",
+            "txt" => ""
+
+        ),
+
+        "french" => array(
+
+            "title" => "",
+            "txt" => '',
+            "mobile" => ''
+
+        ),
+
+    ),
+
+    /* PROJECT */
+    2 => array(
+
+        "about" => array(
+            "title" => "",
+            "txt" => ""
+        ),
+
+        "old-btn" => "",
+
+        "about-old" => array(
+            "title" => "",
+            "txt" => ""
+        ),
+
+        "stat-old" => array(
+            "red" => "",
+            "blue" => "",
+            "yellow" => "",
+            "cyan" => "",
+        ),
+
+        "btn-old" => array(
+            "open" => "",
+            "close" => "",
+        ),
+
+        /* PROJECT */
+
+        "tyroserv" => array(
+            "title" => "",
+            "content" => "",
+            "sub" => "",
+            "btn" => ""
+        ),
+
+        "solidserv" => array(
+            "title" => "",
+            "content" => "",
+            "sub" => "",
+            "btn" => ""
+        ),
+
+        "gamenium" => array(
+            "title" => "",
+            "content" => "",
+            "sub" => "",
+            "btn" => ""
+        ),
+
+        "avra" => array(
+            "title" => "",
+            "content" => "",
+            "sub" => "",
+            "btn" => ""
+        ),
+
+        "tyrostudio" => array(
+            "title" => "",
+            "content" => "",
+            "sub" => "",
+            "btn" => ""
+        ),
+
+        "nexiumiacrm" => array(
+            "title" => "",
+            "content" => "",
+            "sub" => "",
+            "btn" => ""
+        ),
+
+        "vturias" => array(
+            "title" => "",
+            "content" => "",
+            "sub" => "",
+            "btn" => ""
+        ),
+
+        "influnias" => array(
+            "title" => "",
+            "content" => "",
+            "sub" => "",
+            "btn" => ""
+        ),
+
+        /* OLD */
+
+        "wonderlium" => array(
+            "title" => "",
+            "content" => "",
+        ),
+
+        "selemusium" => array(
+            "title" => "",
+            "content" => "",
+        ),
+
+        "tyrodiscord" => array(
+            "title" => "",
+            "content" => "",
+        ),
+
+        "tyroshop" => array(
+            "title" => "",
+            "content" => "",
+        ),
+
+        "duogramme" => array(
+            "title" => "",
+            "content" => "",
+        ),
+
+        "tyrostudent" => array(
+            "title" => "",
+            "content" => "",
+        ),
+
+        "chantium" => array(
+            "title" => "",
+            "content" => "",
+        ),
+
+        "freenium" => array(
+            "title" => "",
+            "content" => "",
+        ),
+
+
+
+    ),
+
+    /* PRESTA */
+    3 => array(
+
+        "web" => array(
+
+            "about" => array(
+                "title" => "",
+                "txt" => ""
+            ),
+
+            "number" => array(
+                "",
+                "",
+                "",
+                "",
+                ""
+            ),
+
+            "form-legend" => array(
+                "prefix-price" => "",
+                "firstTxt" => "",
+                "secondTxt" => "",
+                "contact" => "",
+                "btn" => "",
+            ),
+
+            "form" => array(
+                array(
+                    "name" => "",
+                    "firstTxt" => "",
+                    "secondTxt" => "",
+                ),
+                array(
+                    "name" => "",
+                    "firstTxt" => "",
+                    "secondTxt" => "",
+                ),
+                array(
+                    "name" => "",
+                    "firstTxt" => "",
+                    "secondTxt" => "",
+                )
+            ),
+
+            "rea-legend" => array(
+                "title" => "",
+                "firstBtn" => "",
+                "secondBtn" => "",
+            ),
+
+
+            "rea" => array(
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+
+
+                array(
+                    "name"=>"",
+                    "desc"=>"",
+                ),
+
+            )
+
+
+
+        ),
+
+        "serv" => array(
+
+            "about" => array(
+                "title" => "",
+                "txt" => ""
+            ),
+
+            "number" => array(
+                "",
+                "",
+                "",
+                ""
+            ),
+
+            "title" => "",
+
+            "form" => array(
+                "stockage" => array(
+                    "name" => '',
+                    "list" => ""
+                ),
+                "siteweb" => array(
+                    "name" => '',
+                    "list" => ""
+                ),
+                "vps" => array(
+                    "name" => '',
+                    "list" => ""
+                ),
+                "minecraft" => array(
+                    "name" => '',
+                    "list" => ""
+                ),
+                "fivem" => array(
+                    "name" => '',
+                    "list" => ""
+                )
+            ),
+
+            "other" => array(
+                "top" => "",
+                "next" => "",
+                "btn" => array(
+                    "show" => "",
+                    "none" => ""
+                )
+            ),
+
+            "solidserv" => array(
+                "title" => "",
+                "txt" => "",
+                "btn" => ""
+            ),
+
+
+        ),
+
+
+        "mc" => array(
+
+            "about" => array(
+                "title" => "",
+                "txt" => ""
+            ),
+
+            "number" => array(
+                "",
+                "",
+                "",
+                ""
+            ),
+
+            "event" => array(
+                "title" => "",
+                "txt" => "",
+                "contact" => "",
+                "btn" => ""
+            ),
+
+            "dev" => array(
+                "title" => "",
+                "txt" => "",
+                "btn" => ""
+            ),
+
+            "texture" => array(
+                "title" => "",
+                "txt" => "",
+                "btn" => ""
+            ),
+
+            "serv" => array(
+                "title" => "",
+                "txt" => "",
+                "btn" => ""
+            )
+
+        )
+
+    ),
+
+
+    /* COLLAB */
+    4 => array(
+
+        "part" => array(
+            "title" => "",
+            "txt" => "",
+            "btn" => ""
+        ),
+
+        "people" => array(
+            "title" => "",
+            "txt" => "",
+            "sub" => "",
+            "btn" => ""
+        ),
+
+        "owner" => array(
+            "title" => "",
+            "txt" => "",
+        ),
+
+        "job" => array(
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""
+        ),
+
+        "sponso" => array(
+            "title" => "",
+            "txt" => "",
+        )
+
+    ),
+
+    /* ACCOUNT */
+    6 => array(
+
+        "head" => array(
+            "title-prefix" => "",
+            "sub" => ""
+        ),
+
+        "log" => array(
+            "login" => array(
+                "title" => "",
+                "txt" => "",
+                "btn" => ""
+            ),
+            "signup" => array(
+                "title" => "",
+                "txt" => "",
+                "btn" => ""
+            ),
+            "btn-all" => ""
+        ),
+
+        "about" => array(
+            "title" => "",
+            "txt" => ""
+        ),
+
+        "sync" => array(
+            "title" => "",
+            "txt" => ""
+        ),
+
+        "mail" => array(
+            "title" => "",
+            "txt" => "",
+        ),
+
+        "cloud" => array(
+            "title" => "",
+            "txt" => "",
+        ),
+
+        "doc" => array(
+            "title" => "",
+            "txt" => "",
+        ),
+
+        "presta" => array(
+            "title" => "",
+            "txt" => "",
+        ),
+
+        "jv" => array(
+            "title" => "",
+            "txt" => "",
+        ),
+
+        "serv" => array(
+            "title" => "",
+            "txt" => "",
+        ),
+
+        "enga" => array(
+            "title" => "",
+            "txt" => "",
+        ),
+
+    ),
+
+    /* CONTACT */
+    7 => array(
+
+        "about" => array(
+            "title" => "",
+            "txt" => "",
+        ),
+
+        "form" => array(
+            "title-select" => "",
+            "title-input" => "",
+            "firstname" => "",
+            "lastname" => "",
+            "mail" => "",
+            "num" => "",
+            "msg" => "",
+            "submit" => "",
+            "conditions" => ""
+        ),
+
+        "select" => array(
+            "undefine" => ""
+        ),
+
+        "other" => ""
+
+    ),
+
+    /* MORE */
+    8 => array(
+
+        "about" => array(
+            "title" => "",
+            "txt" => ""
+        )
+
+    ),
+
+    /* 404 */
+    100 => array(
+
+        "title" => "",
+        "btn" => ""
+
+    )
+
+)
+
+
+?>
